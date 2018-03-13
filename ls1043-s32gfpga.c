30ecf4965478ea4555c06c1ccc33b46aa2d7fe99
Bogdan Folea <bogdan.folea@nxp.com>
2018-03-13 15:44:14 +0200

    Implement platform specific memory allocation
    Signed-off-by: Bogdan Folea <bogdan.folea@nxp.com>

--- ls1043-s32gfpga.c
+++ ls1043-s32gfpga.c
@@ -6,46 +6,102 @@
 #include <linux/kernel.h>
 #include <linux/pci.h>
 #include "platform.h"
+#include "s32gfpga-mscm.h"
+
+/* pci-vdev platform specific shared memory configuration */
+#define PCIE_LSHM_OFFSET    (0x10000ul) /* Local Shared Memory Offset */
+#define PCIE_RSHM_OFFSET    (0x00000ul) /* Remote Shared Memory Offset */
+
+/* S32G-FPGA IP Block Base Addresses */
+#define IRAM_BASE     (0x34000000ul) /* Internal Static Random Access Memory */
+#define MSCM_BASE     (0x40198000ul) /* Miscellaneous System Control Module */
+
+/* PCIe Base Address Registers  */
+#define BAR_NUM 0
+
+/**
+ * struct nxp_pfm_priv - platform specific functionalities
+ *
+ * @s32gfpga_base:      pointer to PCIe-mapped S32G-FPGA address space
+ */
+struct nxp_pfm_priv {
+	volatile u8* s32gfpga_base;
+};
 
 int nxp_pfm_init(void **platform)
 {
-	/* platform supported not implemented*/
-	return -EOPNOTSUPP;
+	struct nxp_pfm_priv *priv;
+
+	if (unlikely(!platform))
+		return -EINVAL;
+
+	priv = kzalloc(sizeof(struct nxp_pfm_priv), GFP_KERNEL);
+	if (!priv) {
+		return -EINVAL;
+	}
+
+	*platform = priv;
+	return 0;
 }
 
 void nxp_pfm_free(void *platform)
 {
-	/* platform support not implemented*/
+	struct nxp_pfm_priv *priv;
+
+	if (unlikely(!platform))
+		return;
+
+	priv = (struct nxp_pfm_priv *)platform;
+
+	kfree(priv);
 }
 
 void __iomem *nxp_pfm_alloc_local_shm(void *platform, struct pci_dev *pdev)
 {
-	/* platform support not implemented*/
-	return NULL;
+	struct nxp_pfm_priv *priv;
+	u32 io_len;
+	
+	/* read BAR length and flags */
+	io_len = pci_resource_len(pdev, BAR_NUM);
+	pr_debug("BAR%d len = 0x%x, flags = 0x%lx\n", BAR_NUM, io_len,
+			pci_resource_flags(pdev, BAR_NUM));
+	if (!(pci_resource_flags(pdev, BAR_NUM) & IORESOURCE_MEM_64)) {
+		pr_err("Bad PCI resource\n");
+		return NULL;
+	}
+
+	priv = (struct nxp_pfm_priv *)platform;
+	
+	priv->s32gfpga_base = (u8 *)pci_iomap(pdev, BAR_NUM, io_len);
+	
+	return (void *)priv->s32gfpga_base + IRAM_BASE + PCIE_LSHM_OFFSET;
 }
 
 void nxp_pfm_free_local_shm(void *platform, struct pci_dev *pdev,
 			    void __iomem *addr)
 {
-	/* platform support not implemented*/
+	pci_iounmap(pdev, addr);
 }
 
 void __iomem *nxp_pfm_alloc_remote_shm(void *platform, struct pci_dev *pdev)
 {
-	/* platform support not implemented*/
-	return NULL;
+	struct nxp_pfm_priv *priv;
+	
+	priv = (struct nxp_pfm_priv *)platform;
+	
+	return (void *)priv->s32gfpga_base + IRAM_BASE + PCIE_RSHM_OFFSET;
 }
 
 void nxp_pfm_free_remote_shm(void *platform, struct pci_dev *pdev,
 			     void __iomem *addr)
 {
-	/* platform support not implemented*/
+	/* already done in nxp_pfm_free_local_shm() */
 }
 
 int nxp_pfm_dma_write(void *platform, const void *src_addr,
 			phys_addr_t dest_addr, u32 size)
 {
-	/* platform support not implemented*/
+	/* DMA not supported for this platform */
 	return -EOPNOTSUPP;
 }
 
