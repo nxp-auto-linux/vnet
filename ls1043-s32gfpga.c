4672125891f25a3ca263708aac04330ca2c68e84
Bogdan Folea <bogdan.folea@nxp.com>
2018-03-21 09:39:27 +0200

    Implement platform specific memory write
    Signed-off-by: Bogdan Folea <bogdan.folea@nxp.com>

--- ls1043-s32gfpga.c
+++ ls1043-s32gfpga.c
@@ -10,18 +10,18 @@
 #include "s32gfpga-mscm.h"
 
 /* pci-vdev platform specific interrupt configuration */
-#define PCIE_TX_CPU_TARGET    (0x1u) /* M7 core 0 */
-#define PCIE_RX_CPU_TARGET    (0x8u) /* Root Complex */
-#define PCIE_MSCM_IRQ_ID      (1u) /* Interrupt NVIC ID */
-#define PCIE_MSCM_MSI_ID      (0u) /* Interrupt MSI ID */
+#define PCIE_TX_CPU_TARGET    0x1u /* M7 core 0 */
+#define PCIE_RX_CPU_TARGET    0x8u /* Root Complex */
+#define PCIE_MSCM_IRQ_ID      0u /* Interrupt NVIC ID */
+#define PCIE_MSCM_MSI_ID      0u /* Interrupt MSI ID */
 
 /* pci-vdev platform specific shared memory configuration */
-#define PCIE_LSHM_OFFSET    (0x10000ul) /* Local Shared Memory Offset */
-#define PCIE_RSHM_OFFSET    (0x00000ul) /* Remote Shared Memory Offset */
+#define PCIE_LSHM_OFFSET    0x10000ul /* Local Shared Memory Offset */
+#define PCIE_RSHM_OFFSET    0x00000ul /* Remote Shared Memory Offset */
 
 /* S32G-FPGA IP Block Base Addresses */
-#define IRAM_BASE     (0x34000000ul) /* Internal Static Random Access Memory */
-#define MSCM_BASE     (0x40198000ul) /* Miscellaneous System Control Module */
+#define IRAM_BASE     0x34000000ul /* Internal Static Random Access Memory */
+#define MSCM_BASE     0x40198000ul /* Miscellaneous System Control Module */
 
 /* PCIe Base Address Registers  */
 #define BAR_NUM 0
@@ -30,9 +30,11 @@
  * struct nxp_pfm_priv - platform specific functionalities
  *
  * @s32gfpga_base:      pointer to PCIe-mapped S32G-FPGA address space
+ * @pci_res_start:      physical start address of PCIe resource
  */
 struct nxp_pfm_priv {
 	volatile u8* s32gfpga_base;
+	volatile u64 pci_res_start;
 };
 
 int nxp_pfm_init(void **platform)
@@ -66,28 +68,35 @@ void nxp_pfm_free(void *platform)
 void __iomem *nxp_pfm_alloc_local_shm(void *platform, struct pci_dev *pdev)
 {
 	struct nxp_pfm_priv *priv;
-	u32 io_len;
+	u64 io_len;
 	
 	/* read BAR length and flags */
 	io_len = pci_resource_len(pdev, BAR_NUM);
-	pr_debug("BAR%d len = 0x%x, flags = 0x%lx\n", BAR_NUM, io_len,
+	pr_debug("BAR%d len = 0x%016llx, flags = 0x%lx\n", BAR_NUM, io_len,
 			pci_resource_flags(pdev, BAR_NUM));
+
 	if (!(pci_resource_flags(pdev, BAR_NUM) & IORESOURCE_MEM_64)) {
 		pr_err("Bad PCI resource\n");
 		return NULL;
 	}
 
 	priv = (struct nxp_pfm_priv *)platform;
+
+	priv->pci_res_start = pci_resource_start(pdev, BAR_NUM);
 	
 	priv->s32gfpga_base = (u8 *)pci_iomap(pdev, BAR_NUM, io_len);
 	
-	return (void *)priv->s32gfpga_base + IRAM_BASE + PCIE_LSHM_OFFSET;
+	return (void *)(priv->s32gfpga_base + IRAM_BASE + PCIE_LSHM_OFFSET);
 }
 
 void nxp_pfm_free_local_shm(void *platform, struct pci_dev *pdev,
 			    void __iomem *addr)
 {
-	pci_iounmap(pdev, addr);
+	struct nxp_pfm_priv *priv;
+	
+	priv = (struct nxp_pfm_priv *)platform;
+	
+	pci_iounmap(pdev, (void *)priv->s32gfpga_base);
 }
 
 void __iomem *nxp_pfm_alloc_remote_shm(void *platform, struct pci_dev *pdev)
@@ -96,39 +105,48 @@ void __iomem *nxp_pfm_alloc_remote_shm(void *platform, struct pci_dev *pdev)
 	
 	priv = (struct nxp_pfm_priv *)platform;
 	
-	return (void *)priv->s32gfpga_base + IRAM_BASE + PCIE_RSHM_OFFSET;
+	return (void *)(priv->s32gfpga_base + IRAM_BASE + PCIE_RSHM_OFFSET);
 }
 
 void nxp_pfm_free_remote_shm(void *platform, struct pci_dev *pdev,
 			     void __iomem *addr)
 {
-	/* already done in nxp_pfm_free_local_shm() */
+	return; /* already done in nxp_pfm_free_local_shm() */
 }
 
 int nxp_pfm_dma_write(void *platform, const void *src_addr,
 			phys_addr_t dest_addr, u32 size)
 {
+	struct nxp_pfm_priv *priv;
+	u64 write_offset;
+	
+	priv = (struct nxp_pfm_priv *)platform;
+
+	write_offset = IRAM_BASE + dest_addr - priv->pci_res_start;
+
 	/* DMA not supported for this platform */
-	return -EOPNOTSUPP;
+	memcpy((u8*)priv->s32gfpga_base + write_offset, src_addr, size);
+
+	return 0;
 }
 
 int nxp_pfm_trigger_remote_irq(void *platform)
 {
 	struct nxp_pfm_priv *priv;
-	volatile struct mscm_memmap *MSCM;
+	volatile struct mscm_memmap *mscm;
 	
 	if (unlikely(!platform))
 		return -EINVAL;
 	
 	priv = (struct nxp_pfm_priv *)platform;
 	
-	MSCM = (volatile struct mscm_memmap *)(priv->s32gfpga_base + MSCM_BASE);
+	mscm = (volatile struct mscm_memmap *)(priv->s32gfpga_base + MSCM_BASE);
 	
 	/* generate MSCM core-to-core interrupt */
 	iowrite32(MSCM_IRCPGIR_TLF(0u) /* use CPUTL */
 	          | MSCM_IRCPGIR_CPUTL(PCIE_TX_CPU_TARGET)
 	          | MSCM_IRCPGIR_INTID(PCIE_MSCM_IRQ_ID),
-	          &MSCM->IRCPGIR);
+	          &mscm->IRCPGIR);
 	
 	return 0;
 }
